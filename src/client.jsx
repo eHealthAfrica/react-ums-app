33620309a8853e8e31552fe3eb75558563145159
Rukayat Sadiq <fruqoyya@gmail.com>
2017-08-21 03:25:49 +0100

    mount ums container for in-app use

---
+++ src/client.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import UmsApp from './main';
+
+const mountNode = document.getElementById('content');
+
+const user = {
+  _id: 'org.couchdb.user:dd_admin',
+  _rev: '13-c7519565abbcce68ba3a2341ba3bbc76',
+  name: 'dd_admin',
+  roles: [
+    'admin',
+    'manager',
+    'driver',
+  ],
+  disabled: false,
+  accessLevel: ['all'],
+};
+
+const authorizedRole = ['admin'];
+
+render(<UmsApp user={user} authorizedRole={authorizedRole} />, mountNode);
