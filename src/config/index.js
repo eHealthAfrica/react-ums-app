66d32403ddc3d8b1c91c6130d44d9411c0046f2d
Rukayat Sadiq <fruqoyya@gmail.com>
2017-08-29 01:13:42 +0100

    modify api base url

--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,20 @@
 const BASE_URL = API_HOST;
 
 const config = {
-  LOGGED_IN_USER_URL: `${BASE_URL}users/me`,
-  AUTH_URL: `${BASE_URL}auth`,
-  LOCATION_URL: `${BASE_URL}locations`,
-  USERS_URL: `${BASE_URL}users/all`,
-  ROLES_URL: `${BASE_URL}roles`,
-  USER_URL: `${BASE_URL}users/get`,
-  CREATE_USER_URL: `${BASE_URL}users/new`,
-  EDIT_USER_URL: `${BASE_URL}users/edit`,
-  CHANGE_PASSWORD_URL: `${BASE_URL}users/update-password`,
-  CHANGE_USER_PASSWORD_URL: `${BASE_URL}users/update-password-admin`,
+  LOGGED_IN_USER_URL: `${BASE_URL}/ums/users/me`,
+  AUTH_URL: `${BASE_URL}/ums/auth`,
+  LOCATION_URL: `${BASE_URL}/ums/locations`,
+  USERS_URL: `${BASE_URL}/ums/users/all`,
+  ROLES_URL: `${BASE_URL}/ums/roles`,
+  USER_URL: `${BASE_URL}/ums/users/get`,
+  CREATE_USER_URL: `${BASE_URL}/ums/users/new`,
+  EDIT_USER_URL: `${BASE_URL}/ums/users/edit`,
+  CHANGE_PASSWORD_URL: `${BASE_URL}/ums/users/update-password`,
+  CHANGE_USER_PASSWORD_URL: `${BASE_URL}/ums/users/update-password-admin`,
   DEFAULT_ROLES: ['View Dashboard'],
-  REQUEST_PASSWORD_RESET_URL: `${BASE_URL}users/request-password-reset`,
-  GET_RESET_TOKEN_URL: `${BASE_URL}users/reset-token`,
-  RESET_PASSWORD_URL: `${BASE_URL}users/reset-password`,
+  REQUEST_PASSWORD_RESET_URL: `${BASE_URL}/ums/users/request-password-reset`,
+  GET_RESET_TOKEN_URL: `${BASE_URL}/ums/users/reset-token`,
+  RESET_PASSWORD_URL: `${BASE_URL}/ums/users/reset-password`,
 };
 
 export default config;
